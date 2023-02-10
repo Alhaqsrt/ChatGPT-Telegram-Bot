1f58b51b5b289e23856403b598309d8936a58fee
Alhaqsrt <124993247+Alhaqsrt@users.noreply.github.com>
2023-02-10 16:33:51 +0530

    AlhaqChatgpt

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "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"
 else:
     tgkey = tgenv
 print(tgenv)
@@ -35,8 +35,8 @@ running = False
 cache = None
 qcache = None
 chat_log = None
-botname = 'Harshit ethic'
-username = 'harshitethic_bot'
+botname = 'chatgpt Alhaq'
+username = 'ChatGpt_Alhaqsrt_bot'
 # Max chat log length (A token is about 4 letters and max tokens is 2048)
 max = int(3000)
 
